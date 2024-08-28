340982e1de9577bacade1ac2bd1895d1adfe112e
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-08-28 13:08:28 +0300

    split into modules

---
+++ src/components.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct Lapis {
+    pub buffer: String,
+    pub input: String,
+    pub settings: bool,
+    pub fmap: HashMap<String, f32>,
+    //vmap: HashMap<String, Vec<f32>>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use eframe::egui;
-use std::collections::HashMap;
-use syn::*;
+
+mod components;
+mod eval;
+use {components::*, eval::*};
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
@@ -8,15 +10,6 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native("awawawa", options, Box::new(|_| Ok(Box::<Lapis>::default())))
-}
-
-#[derive(Default)]
-struct Lapis {
-    buffer: String,
-    input: String,
-    settings: bool,
-    fmap: HashMap<String, f32>,
-    //vmap: HashMap<String, Vec<f32>>,
 }
 
 impl eframe::App for Lapis {
@@ -80,95 +73,5 @@ impl eframe::App for Lapis {
                 );
             });
         });
-    }
-}
-
-fn eval(lapis: &mut Lapis) {
-    if let Ok(stmt) = parse_str::<Stmt>(&lapis.input) {
-        lapis.buffer.push('\n');
-        lapis.buffer.push_str(&lapis.input);
-        lapis.input.clear();
-        println!("{:#?}", stmt);
-        if let Stmt::Expr(Expr::Block(expr), _) = stmt {
-            for stmt in expr.block.stmts {
-                eval_stmt(stmt, lapis);
-            }
-        } else {
-            eval_stmt(stmt, lapis);
-        }
-    }
-}
-
-fn eval_stmt(s: Stmt, lapis: &mut Lapis) {
-    match s {
-        Stmt::Local(expr) => {
-            if let Pat::Ident(i) = expr.pat {
-                let k = i.ident.to_string();
-                if let Some(expr) = expr.init {
-                    let expr = *expr.expr;
-                    let v = match expr {
-                        Expr::Lit(expr) => lit_float(&expr.lit),
-                        Expr::Binary(expr) => bin_expr_float(&expr),
-                        Expr::Paren(expr) => paren_expr_float(&expr.expr),
-                        _ => None,
-                    };
-                    if let Some(v) = v {
-                        lapis.fmap.insert(k, v);
-                    }
-                }
-            }
-        }
-        Stmt::Expr(expr, _) => match expr {
-            Expr::Path(expr) => {
-                let segments = &expr.path.segments;
-                if let Some(s) = segments.first() {
-                    let k = s.ident.to_string();
-                    lapis.buffer.push_str(&format!("\n>{:?}", lapis.fmap.get(&k)));
-                }
-            }
-            Expr::Binary(expr) => {
-                println!("{:?}", bin_expr_float(&expr));
-            }
-            _ => {}
-        },
-        _ => {}
-    }
-}
-
-fn bin_expr_float(expr: &ExprBinary) -> Option<f32> {
-    let left = match *expr.left.clone() {
-        Expr::Lit(expr) => lit_float(&expr.lit)?,
-        Expr::Binary(expr) => bin_expr_float(&expr)?,
-        Expr::Paren(expr) => paren_expr_float(&expr.expr)?,
-        _ => return None,
-    };
-    let right = match *expr.right.clone() {
-        Expr::Lit(expr) => lit_float(&expr.lit)?,
-        Expr::Binary(expr) => bin_expr_float(&expr)?,
-        Expr::Paren(expr) => paren_expr_float(&expr.expr)?,
-        _ => return None,
-    };
-    match expr.op {
-        BinOp::Sub(_) => Some(left - right),
-        BinOp::Div(_) => Some(left / right),
-        BinOp::Mul(_) => Some(left * right),
-        BinOp::Add(_) => Some(left + right),
-        _ => None,
-    }
-}
-
-fn lit_float(expr: &Lit) -> Option<f32> {
-    match expr {
-        Lit::Float(expr) => expr.base10_parse::<f32>().ok(),
-        Lit::Int(expr) => expr.base10_parse::<f32>().ok(),
-        _ => None,
-    }
-}
-
-fn paren_expr_float(expr: &Expr) -> Option<f32> {
-    match expr {
-        Expr::Lit(expr) => lit_float(&expr.lit),
-        Expr::Binary(expr) => bin_expr_float(expr),
-        _ => None,
     }
 }
