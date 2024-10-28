0912f21580ae0179f1271438de46ff8751457571
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-10-28 20:27:53 +0300

    edits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use cpal::traits::{DeviceTrait, HostTrait};
 use eframe::egui::*;
 
@@ -6,6 +8,7 @@ mod components;
 mod eval;
 use {audio::*, components::*, eval::*};
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: ViewportBuilder {
@@ -17,6 +20,45 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native("awawawa", options, Box::new(|_| Ok(Box::new(Lapis::new()))))
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window().expect("No window").document().expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(canvas, web_options, Box::new(|_| Ok(Box::new(Lapis::new()))))
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
 impl eframe::App for Lapis {
